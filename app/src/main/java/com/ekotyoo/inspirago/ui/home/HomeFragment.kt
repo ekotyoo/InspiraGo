bf4765e50ab4f0267bf6ca00ab741199bbb41d28
ekotyoo <ekeprasetyo@gmail.com>
2022-04-02 21:48:06 +0700

    Add Glide dependency

--- app/src/main/java/com/ekotyoo/inspirago/ui/home/HomeFragment.kt
+++ app/src/main/java/com/ekotyoo/inspirago/ui/home/HomeFragment.kt
@@ -5,14 +5,34 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bumptech.glide.Glide
 import com.ekotyoo.inspirago.R
+import com.ekotyoo.inspirago.databinding.FragmentHomeBinding
+
+private const val dummyImageUrl = "https://images.unsplash.com/photo-1648780693516-3ff39ff65d1e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80"
 
 class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_home, container, false)
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        Glide.with(binding.root)
+            .load(dummyImageUrl)
+            .into(binding.ivQuote)
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
