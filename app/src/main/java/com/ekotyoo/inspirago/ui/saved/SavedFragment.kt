3a37c99c2f167bf6efe2fca49fb738145ab0321a
ekotyoo <ekeprasetyo@gmail.com>
2022-04-03 08:27:26 +0700

    Create fragment_saved layout

--- app/src/main/java/com/ekotyoo/inspirago/ui/saved/SavedFragment.kt
+++ app/src/main/java/com/ekotyoo/inspirago/ui/saved/SavedFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.ekotyoo.inspirago.databinding.FragmentSavedBinding
 
 class SavedFragment : Fragment() {
@@ -18,6 +19,13 @@ class SavedFragment : Fragment() {
     ): View {
         _binding = FragmentSavedBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btnBack.setOnClickListener {
+            findNavController().navigateUp()
+        }
     }
 
     override fun onDestroyView() {
